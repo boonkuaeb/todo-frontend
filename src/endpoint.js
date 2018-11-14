7248c2e3b221a54413410370f8b0ce4b9ec95044
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-14 20:25:57 +0700

    add prod

--- src/endpoint.js
+++ src/endpoint.js
@@ -2,9 +2,9 @@
 var endpoint = {
     loginURL: "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1619902299&redirect_uri=https://bk-todo-app.herokuapp.com/auth&scope=profile&state=",
     getProfile: "https://api.line.me/v2/profile",
-    todoApiURL: "https://f04aac9b.ngrok.io/api/v1/todo/user/",
-    todoApiUpdateURL: "https://f04aac9b.ngrok.io/api/v1/todo/",
-    getTokenURL: "https://f04aac9b.ngrok.io/api/auth"
+    todoApiURL: "https://todorest.herokuapp.com/api/v1/todo/user/",
+    todoApiUpdateURL: "https://todorest.herokuapp.com/api/v1/todo/",
+    getTokenURL: "https://todorest.herokuapp.com/api/auth"
 };
 
 
