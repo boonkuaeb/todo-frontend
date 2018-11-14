75b292678ac0f29ea80189e2b043a920b4e78611
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-14 20:02:25 +0700

    change endpoint to https

--- src/endpoint.js
+++ src/endpoint.js
@@ -2,9 +2,9 @@
 var endpoint = {
     loginURL: "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1619902299&redirect_uri=https://bk-todo-app.herokuapp.com/auth&scope=profile&state=",
     getProfile: "https://api.line.me/v2/profile",
-    todoApiURL: "http://f04aac9b.ngrok.io/api/v1/todo/user/",
-    todoApiUpdateURL: "http://f04aac9b.ngrok.io/api/v1/todo/",
-    getTokenURL: "http://f04aac9b.ngrok.io/api/auth"
+    todoApiURL: "https://f04aac9b.ngrok.io/api/v1/todo/user/",
+    todoApiUpdateURL: "https://f04aac9b.ngrok.io/api/v1/todo/",
+    getTokenURL: "https://f04aac9b.ngrok.io/api/auth"
 };
 
 
