552cd7ee3af97b05c23ef9b247787da71ccf2430
Boonkuae Boonsutta <boonkuae.boo@ascendcorp.com>
2018-11-18 00:23:51 +0700

    Fix redirect URL

--- src/endpoint.js
+++ src/endpoint.js
@@ -1,6 +1,6 @@
 
 var endpoint = {
-    loginURL: "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1619902299&redirect_uri=https://line-todo-api.herokuapp.com/auth&scope=profile&state=",
+    loginURL: "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=1619902299&redirect_uri=https://line-todo-manage.herokuapp.com/auth&scope=profile&state=",
     getProfile: "https://api.line.me/v2/profile",
     todoApiURL: "https://line-todo-api.herokuapp.com/api/v1/todo/user/",
     todoApiUpdateURL: "https://line-todo-api.herokuapp.com/api/v1/todo/",
